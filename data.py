7e40539b3668782b775152ad0980ea65653edaf2
newby-by <practikum10@gmail.com>
2026-02-20 20:26:21 +0300

    Refactor urls by split it

--- data.py
+++ data.py
@@ -1,4 +1,5 @@
-URL = 'https://qa-desk.stand.praktikum-services.ru/'
+BASE_URL = 'https://qa-desk.stand.praktikum-services.ru/'
+URL_REGISTRATION = BASE_URL + 'regiatration/'
 
 # Start page
 TITLE = 'React App'
--- tests/test_start_page.py
+++ tests/test_start_page.py
@@ -4,6 +4,7 @@ import data
 class TestStartPage:
 
     def test_start_page_is_available(self, driver):
-        driver.get(data.URL)
+        driver.get(data.BASE_URL)
 
-        assert driver.current_url == data.URL and driver.title == data.TITLE
+        assert (driver.current_url == data.BASE_URL and 
+                driver.title == data.TITLE)
