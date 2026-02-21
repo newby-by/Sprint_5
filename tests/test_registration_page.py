ab3cd9f7c57c6ecb7510187889286f39c936b485
newby-by <practikum10@gmail.com>
2026-02-21 17:24:08 +0300

    Refactor add set up form into tests

--- tests/test_registration_page.py
+++ tests/test_registration_page.py
@@ -5,19 +5,20 @@ class TestRegistrationPage:
     
     def test_registration_with_correct_credentials(
             self,
-            fill_sign_up_form_with_correct_data
+            sign_up_page
     ):
         """Registration with correct credentials."""
-        actual_user_name = fill_sign_up_form_with_correct_data.get_user_name()
+        sign_up_page.fill_sign_up_form(data.user.user_with_correct_credentials)
+        actual_user_name = sign_up_page.get_user_name()
 
         assert actual_user_name == data.USER_NAME
     
     def test_registration_with_incorrect_email(
             self,
-            fill_sign_up_form_with_incorrect_email
+            sign_up_page
     ):
         """Registration with INcorrect email."""
-        actual_error_message = (fill_sign_up_form_with_incorrect_email.
-                                get_error_message_email_field())
+        sign_up_page.fill_sign_up_form(data.user.user_with_incorrect_email)
+        actual_error_message = sign_up_page.get_error_message_email_field()
 
         assert actual_error_message == data.ERROR_MESSAGE_EMAIL_FIELD
