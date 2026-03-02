14a858916c01a72170eb26718683d533175608c8
newby-by <practikum10@gmail.com>
2026-03-02 15:05:11 +0300

    Remove useless yield from fixtures

--- conftest.py
+++ conftest.py
@@ -21,28 +21,28 @@ def driver():
 def main_page(driver):
     start_page = MainPage(driver)
     start_page.open(data.BASE_URL)
-    yield start_page
+    return start_page
 
 
 @pytest.fixture(scope='function')
 def sign_in_page(main_page):
     main_page.go_to_sign_in_form()
-    yield main_page
+    return main_page
 
 
 @pytest.fixture(scope='function')
 def sign_up_page(sign_in_page):
     sign_in_page.go_to_sign_up_form()
-    yield sign_in_page
+    return sign_in_page
 
 
 @pytest.fixture(scope='function')
 def registration_user(sign_up_page):
     sign_up_page.fill_sign_up_form(data.user.new)
-    yield sign_up_page
+    return sign_up_page
 
 
 @pytest.fixture(scope='function')
 def logout(registration_user):
     registration_user.logout()
-    yield registration_user
+    return registration_user
