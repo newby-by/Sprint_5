45303a3e960f0c4850ddefed22fdd621732d2366
newby-by <practikum10@gmail.com>
2026-03-03 14:10:20 +0300

    Refactor by adding classmethod get price

--- pages/account_user_page.py
+++ pages/account_user_page.py
@@ -12,8 +12,14 @@ class AccountUserPage(BasePage):
 
         actual_name = self.get_text(AccountUserLocators.NAME_GOODS)
         actual_city = self.get_text(AccountUserLocators.CITY_GOODS)
-        actual_price = self.get_text(AccountUserLocators.PRICE_GOODS).split()[0]
+        actual_price_text = self.get_text(
+            AccountUserLocators.PRICE_GOODS
+        )
 
         return (actual_name == name and
                 actual_city == city and
-                int(actual_price) == price)
+                int(self.get_price(actual_price_text)) == price)
+
+    @classmethod
+    def get_price(cls, text):
+        return text.split()[0]
